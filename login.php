4fd56774e601b4a54c273499f8bf11347b946703
YeshaShah13 <yeshashah1334@gmail.com>
2025-08-23 18:34:54 +0530

    login changes

--- login.php
+++ login.php
@@ -200,7 +200,7 @@ if (isset($_POST['email'])) {
               <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zm-1 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
               </svg>
-              <span><strong>Test Account:</strong> Email: <code>admin@expense.com</code> | Password: <code>password</code></span>
+              
             </div>
           </div>
           
